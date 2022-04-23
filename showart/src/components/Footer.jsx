260ffe5bcb681667c04e11a6992695b1b228c60b
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-23 18:02:04 +0530

    footer done

--- showart/src/components/Footer.jsx
+++ showart/src/components/Footer.jsx
@@ -1,46 +1,125 @@
-import {Link} from 'react-router-dom';
-//import {useState, useEffect,useRef} from 'react'
-//import Footer from './components/Footer' 
-const Footer = (props)=>{
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
 
-return(
-<>
-
-<div className="justify-between items-center px-24 py-12 text-gray-300 align-middle bg-black md:flex">
-
-    <div  className="flex mx-3 justify-center">
-        <img className="content-center" src="https://raw.githubusercontent.com/kushagra-01/random/main/f08c0e10-ba1d-470c-aee8-87dae14d1f4d.jpg" alt="" />
-    </div>
-    <div className="md:mr-40 md:mx-3">
-        <div className="grid text-center grid-1 md:grid-cols-4 lg:grid-cols-4">
-
-        <Link className="my-1 hover:underline mt-2" to="">© ShowArt</Link>
-        <Link className="my-1 hover:underline" to="">Privacy Policy</Link>
-        <Link className="my-1 hover:underline" to="">Cookie Policy</Link>
-        <Link className="my-1 hover:underline" to="">Terms of Service</Link>
-        <Link className="my-1 hover:underline" to="">Do Not Sell My Personal Information</Link>
-        <Link className="my-1 hover:underline" to="">Refunds</Link>
+export default function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Life Is Too Short For Artist So Better Get connected </span>
         </div>
-        <div className="text-[12px] text-gray-300 text-center my-1">ShowArt Inc. | All rights reserved. All trademark referenced herein are properties of their respective owners. The ArtShow is operated by 5bits.</div>
-    </div>
-    <div className="mx-3">
-        <select name="" id="" className="p-2 text-white bg-black focus:bg-black hover:bg-gray-500 outline-none">
-            <option value="">English</option>
-            <option value="">Spanish</option>
-            <option value="">French</option>
-        </select>
-        
-    </div>
-    <div className="flex mx-3 justify-center">
-        <img src="https://raw.githubusercontent.com/kushagra-01/random/main/f08c0e10-ba1d-470c-aee8-87dae14d1f4d.jpg" alt="" className="content-center mt-3 w-[3rem] md:max-w-[7rem] md:max-h-[5rem]" />
-    </div>
-    
-</div>
+
+        <div>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-facebook-f'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-twitter'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-google'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-instagram'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-github'></i>
+          </a>
+        </div>
+      </section>
+
+      <section className=''>
+        <div className='container text-center text-md-start mt-5'>
+          <div className='row mt-3'>
+            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <i className='fas fa-gem me-3'></i>ArtShow
+              </h6>
+              <p>
+               
+              Color! What a deep and mysterious language. It is the language of dreams.
 
 
-</>
+              </p>
+            </div>
 
-)
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Tech Stacks</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  NodeJs
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  React
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  ExpressJs
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  MongoDB
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <i className='fas fa-home me-3'></i> INDIA
+              </p>
+              <p>
+                <i className='fas fa-envelope me-3'></i>
+                k@k.com
+              </p>
+              <p>
+                <i className='fas fa-phone me-3'></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className='fas fa-print me-3'></i> + 01 234 567 89
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2021 Copyright:
+        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+          ArtShow
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
-
-export default Footer;
--- showart/src/index.js
+++ showart/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 
 import { Provider as ReduxProvider } from 'react-redux';
